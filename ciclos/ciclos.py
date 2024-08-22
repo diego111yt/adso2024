6b01bae330cb77f6d263a9f0bc4d534cbd79cbf0
diego ramirez <jamdiego24@gmail.com>
2024-08-22 09:37:50 -0500

    version2

---
+++ ciclos/ciclos.py
@@ -0,0 +1,5 @@
+a=int(input("digite hasta que numero quiere que cuente "))
+b=0
+for i in range(a):
+    b+=1
+    print(b)
---
+++ condiciones/condiciones.py
@@ -0,0 +1,8 @@
+a=input("""Cual es el resultado de la divicion de 40 entre 2
+1. 10
+2. 30
+3. 20""")
+if a=="2":
+    print("correcto")
+else:
+    print("incorrecto")
